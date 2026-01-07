d1e30d23b9c4f98dad913098f20f363e8ac314df
Scrabba87 <SIMONLUCA.SCRAVAGLIERI@ROYALUNIBREW.COM>
2026-01-07 14:24:19 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(page_title="Promo Planner 2026", layout="wide")
+
+st.title("Promo Planner 2026")
+st.subheader("Cliente: Esselunga")
+
+st.markdown("### Seleziona cosa vuoi fare")
+choice = st.radio(
+    "Scegli una sezione",
+    ["LAST YEAR VIEW", "INSERIMENTO PROMO"],
+    horizontal=True
+)
+
+if choice == "LAST YEAR VIEW":
+    st.info("Qui vedrai cosa è stato promosso lo scorso anno (placeholder)")
+else:
+    st.success("Qui inserirai una nuova promo (step successivo)")
